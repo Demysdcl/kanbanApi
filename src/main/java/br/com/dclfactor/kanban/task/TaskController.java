4c5427b9404b07c6340d79813379b4af360eef31
demys <demys.lima@fpf.br>
2018-03-04 13:00:49 -0400

    Task controle

--- src/main/java/br/com/dclfactor/kanban/task/TaskController.java
+++ src/main/java/br/com/dclfactor/kanban/task/TaskController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/list")
+@RequestMapping("/tasks/list")
 public class TaskController {
 
     @Autowired
